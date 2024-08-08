47d8ab08c5e0a133da2fc333823b5a15f417d3a7
proPianist1124 <phantomforces11242009@gmail.com>
2024-08-08 15:45:06 -0700

    fixed error handling

--- src/routes/api/auth/discord/+server.ts
+++ src/routes/api/auth/discord/+server.ts
@@ -5,48 +5,52 @@ import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } from "$env/static/private";
 import { db } from "$lib/postgres";
 
 export async function GET({ url, cookies }) {
-    const formData = new URLSearchParams({
-        client_id: DISCORD_CLIENT_ID,
-        client_secret: DISCORD_CLIENT_SECRET,
-        grant_type: "authorization_code",
-        code: String(url.searchParams.get("code")),
-        redirect_uri: "http://localhost:3000/api/auth/discord"
-    });
+    if (url.searchParams.get("code")) {
+        const formData = new URLSearchParams({
+            client_id: DISCORD_CLIENT_ID,
+            client_secret: DISCORD_CLIENT_SECRET,
+            grant_type: "authorization_code",
+            code: String(url.searchParams.get("code")),
+            redirect_uri: "http://localhost:3000/api/auth/discord"
+        });
 
-    const output = await axios.post("https://discord.com/api/v10/oauth2/token",
-        formData, {
+        const output = await axios.post("https://discord.com/api/v10/oauth2/token",
+            formData, {
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded"
             }
         }
-    );
+        );
 
-    if (output.data) {
-        const access = output.data.access_token;
+        if (output.data) {
+            const access = output.data.access_token;
 
-        const userInfo = await axios.get("https://discord.com/api/v10/users/@me", {
-            headers: {
-                "Authorization": `Bearer ${access}`
+            const userInfo = await axios.get("https://discord.com/api/v10/users/@me", {
+                headers: {
+                    "Authorization": `Bearer ${access}`
+                }
+            });
+
+            const user = await db`SELECT id FROM yasss_users WHERE email = ${userInfo.data.email};`;
+
+            if (user.length !== 0) {
+                cookies.set("token", user[0].id, {
+                    path: "/"
+                });
+            } else {
+                const id = uuid();
+
+                await db`INSERT INTO yasss_users (id, email, username) VALUES (${id}, ${userInfo.data.email}, ${userInfo.data.username});`;
+
+                cookies.set("token", id, {
+                    path: "/"
+                });
             }
-        });
 
-        const user = await db`SELECT id FROM yasss_users WHERE email = ${userInfo.data.email};`;
-
-        if (user.length !== 0) {
-            cookies.set("token", user[0].id, {
-                path: "/"
-            });
+            redirect(302, "/");
         } else {
-            const id = uuid();
-
-            await db`INSERT INTO yasss_users (id, email, username) VALUES (${id}, ${userInfo.data.email}, ${userInfo.data.username});`;
-
-            cookies.set("token", id, {
-                path: "/"
-            });
+            return new Response("Invalid code.");
         }
-
-        redirect(302, "/");
     } else {
         return new Response("Invalid code.");
     }
